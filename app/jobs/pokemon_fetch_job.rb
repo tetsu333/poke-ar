2663a90b7e8bc1804b5ff814d58a8fb06027a812
tetsu <cda07c5117@gmail.com>
2024-01-13 00:05:02 +0900

    不要コード削除等

--- app/jobs/pokemon_fetch_job.rb
+++ app/jobs/pokemon_fetch_job.rb
@@ -2,35 +2,33 @@ class PokemonFetchJob < ApplicationJob
   queue_as :default
 
   def perform(pokedex_number_or_name)
-    begin
-      pokemon_data = PokeapiClient.fetch_pokemon(pokedex_number_or_name)
-      pokedex_number = pokemon_data["id"]
-      pokemon = Pokemon.find_or_initialize_by(pokedex_number: pokedex_number)
+    pokemon_data = PokeapiClient.fetch_pokemon(pokedex_number_or_name)
+    pokedex_number = pokemon_data["id"]
+    pokemon = Pokemon.find_or_initialize_by(pokedex_number: pokedex_number)
 
-      species_url = pokemon_data.dig("species", "url")
-      species_data = PokeapiClient.fetch_japanese_name(species_url)
-      names = species_data["names"]
-      name = names.find { |v| v["language"]["name"] == "ja" }["name"]
+    species_url = pokemon_data.dig("species", "url")
+    species_data = PokeapiClient.fetch_japanese_name(species_url)
+    names = species_data["names"]
+    name = names.find { |v| v["language"]["name"] == "ja" }["name"]
 
-      image_urls = [
-        pokemon_data.dig("sprites", "other", "home", "front_default"),
-        pokemon_data.dig("sprites", "other", "home", "front_shiny"),
-        pokemon_data.dig("sprites", "other", "official-artwork", "front_default"),
-        pokemon_data.dig("sprites", "other", "official-artwork", "front_shiny")
-      ].compact
-      image_urls.delete(pokemon.image_url)
-      image_url = image_urls.sample
-  
-      pokemon.assign_attributes(
-        pokedex_number: pokedex_number,
-        name: name,
-        image_url: image_url
-      )
-      pokemon.save
-      pokemon
-    rescue => e
-      Rails.logger.error "Error fetching Pokemon data: #{e.message}"
-      Pokemon.new
-    end
+    image_urls = [
+      pokemon_data.dig("sprites", "other", "home", "front_default"),
+      pokemon_data.dig("sprites", "other", "home", "front_shiny"),
+      pokemon_data.dig("sprites", "other", "official-artwork", "front_default"),
+      pokemon_data.dig("sprites", "other", "official-artwork", "front_shiny")
+    ].compact
+    image_urls.delete(pokemon.image_url)
+    image_url = image_urls.sample
+
+    pokemon.assign_attributes(
+      pokedex_number: pokedex_number,
+      name: name,
+      image_url: image_url
+    )
+    pokemon.save!
+    pokemon
+  rescue => e
+    Rails.logger.error "Error fetching Pokemon data: #{e.message}"
+    Pokemon.new
   end
 end
