dec06bf85971157d8b946810e22d7512a20bad47
tetsu <cda07c5117@gmail.com>
2024-01-12 19:59:01 +0900

    画像の種類増加

--- app/jobs/pokemon_fetch_job.rb
+++ app/jobs/pokemon_fetch_job.rb
@@ -10,7 +10,13 @@ class PokemonFetchJob < ApplicationJob
       species_data = PokeapiClient.fetch_japanese_name(species_url)
       names = species_data["names"]
       name = names.find { |v| v["language"]["name"] == "ja" }["name"]
-      image_url = pokemon_data.dig("sprites", "other", "home", "front_default")
+      image_urls = [
+        pokemon_data.dig("sprites", "other", "home", "front_default"),
+        pokemon_data.dig("sprites", "other", "home", "front_shiny"),
+        pokemon_data.dig("sprites", "other", "official-artwork", "front_default"),
+        pokemon_data.dig("sprites", "other", "official-artwork", "front_shiny")
+      ].compact
+      image_url = image_urls.sample
   
       pokemon.assign_attributes(
         pokedex_number: pokedex_number,
