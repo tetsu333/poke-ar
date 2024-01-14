58d49d8859ffcc9544005d0f7b73ac4dbeca53e0
tetsu <cda07c5117@gmail.com>
2024-01-14 19:59:28 +0900

    リファクタリング

--- app/jobs/pokemon_fetch_job.rb
+++ app/jobs/pokemon_fetch_job.rb
@@ -2,33 +2,47 @@ class PokemonFetchJob < ApplicationJob
   queue_as :default
 
   def perform(pokedex_number_or_name)
-    pokemon_data = PokeapiClient.fetch_pokemon(pokedex_number_or_name)
+    pokemon_data = fetch_pokemon_data(pokedex_number_or_name)
+    save_pokemon(pokemon_data)
+  rescue StandardError => e
+    handle_error(e)
+  end
+
+  private
+
+  def fetch_pokemon_data(pokedex_number_or_name)
+    PokeapiClient.fetch_pokemon(pokedex_number_or_name)
+  end
+
+  def save_pokemon(pokemon_data)
     pokedex_number = pokemon_data["id"]
     pokemon = Pokemon.find_or_initialize_by(pokedex_number: pokedex_number)
+    name = fetch_japanese_name(pokemon_data.dig("species", "url"))
+    image_url = select_image_url(pokemon_data, pokemon.image_url)
+    pokemon.assign_attributes(pokedex_number: pokedex_number, name: name, image_url: image_url)
+    pokemon.save!
+    pokemon
+  end
 
-    species_url = pokemon_data.dig("species", "url")
+  def fetch_japanese_name(species_url)
     species_data = PokeapiClient.fetch_japanese_name(species_url)
     names = species_data["names"]
-    name = names.find { |v| v["language"]["name"] == "ja" }["name"]
+    names.find { |v| v["language"]["name"] == "ja" }["name"]
+  end
 
+  def select_image_url(pokemon_data, current_image_url)
     image_urls = [
       pokemon_data.dig("sprites", "other", "home", "front_default"),
       pokemon_data.dig("sprites", "other", "home", "front_shiny"),
       pokemon_data.dig("sprites", "other", "official-artwork", "front_default"),
       pokemon_data.dig("sprites", "other", "official-artwork", "front_shiny")
     ].compact
-    image_urls.delete(pokemon.image_url)
-    image_url = image_urls.sample
+    image_urls.delete(current_image_url)
+    image_urls.sample
+  end
 
-    pokemon.assign_attributes(
-      pokedex_number: pokedex_number,
-      name: name,
-      image_url: image_url
-    )
-    pokemon.save!
-    [pokemon]
-  rescue => e
-    Rails.logger.error "Error fetching Pokemon data: #{e.message}"
-    [Pokemon.new, e]
+  def handle_error(error)
+    Rails.logger.error "Error fetching Pokemon data: #{error.message}"
+    [Pokemon.new, error.message]
   end
 end
