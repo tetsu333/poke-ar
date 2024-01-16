302c1c628d4f2b75e902940b17770698d8c4a94d
tetsu <cda07c5117@gmail.com>
2024-01-17 00:54:20 +0900

    DB設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,17 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /myapp/tmp/pids/server.pid
 
+# production環境の場合のみ
+if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rails assets:precompile
+  # --------------------------------------
+  # 本番環境（AWS ECS）への初回デプロイ時に利用
+  # 初回デプロイ後にコメントアウトする
+  bundle exec rails db:create
+  # --------------------------------------
+  # マイグレーション処理
+  bundle exec rails db:migrate
+fi
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
