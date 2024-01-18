0b2840a0bc31b20534c18c0c4b172c90c8f56621
tetsu <cda07c5117@gmail.com>
2024-01-19 02:11:54 +0900

    メッセージ表示修正

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -25,7 +25,7 @@ class PokemonsController < ApplicationController
     return redirect_to pokemons_path, alert: "既に登録済みです" if Pokemon.find_by(pokedex_number: pokemon_params[:pokedex_number])
     @pokemon, message = PokemonFetchJob.perform_now(pokemon_params[:pokedex_number])
     if @pokemon.persisted?
-      redirect_to pokemons_path, notice: "#{@pokemon.name}が登録されました。"
+      redirect_to pokemons_path, notice: "#{@pokemon.name}が登録されました"
     else
       redirect_to pokemons_path, alert: message
     end
@@ -35,7 +35,7 @@ class PokemonsController < ApplicationController
   def update
     @pokemon, message = PokemonFetchJob.perform_now(pokemon_params[:pokedex_number])
     if @pokemon.persisted?
-      redirect_to pokemons_path, notice: "新しい#{@pokemon.name}に交換されました。"
+      redirect_to pokemons_path, notice: "新しい#{@pokemon.name}に交換されました"
     else
       redirect_to pokemons_path, alert: message
     end
@@ -44,7 +44,7 @@ class PokemonsController < ApplicationController
   # DELETE /pokemons/1
   def destroy
     @pokemon.destroy!
-    redirect_to pokemons_path, notice: "#{@pokemon.name}を逃しました。"
+    redirect_to pokemons_path, notice: "#{@pokemon.name}を逃しました"
   end
 
   private
